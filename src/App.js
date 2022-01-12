c7902f7d7ffeb0172454e940276f323afa263739
chinwei.hsu <chinweihsu.1991@gmail.com>
2022-01-12 16:50:21 +0800

    update style, href

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ function App() {
       <header className="App-header">
         <div
           className="fb-share-button"
-          dataHref="https://www.facebook.com/watch/?v=1666963653648574&amp;extid=NS-UNK-UNK-UNK-IOS_GK0T-GK1C"
-          dataLayout="button"
-          dataSize="large"
+          data-href="https://www.facebook.com/watch/?v=1666963653648574&amp;extid=NS-UNK-UNK-UNK-IOS_GK0T-GK1C"
+          data-layout="button_count"
+          data-size="large"
         >
-            <a
-              target="_blank"
-              href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fwatch%2F%3Fv%3D1666963653648574%26extid%3DNS-UNK-UNK-UNK-IOS_GK0T-GK1C&amp;src=sdkpreparse"
-              className="fb-xfbml-parse-ignore"
-            >
-              Share
-            </a>
+          <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2Fwatch%2F%3Fv%3D1666963653648574%26extid%3DNS-UNK-UNK-UNK-IOS_GK0T-GK1C&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">
+            Share
+          </a>
         </div>
       </header>
     </div>
